f1858731fb57beb645f95a059fdd05d1987e4af1
abchintala <achintala@brierly.com>
2019-08-20 01:11:19 -0500

    uodated kube versions

--- initial.sh
+++ initial.sh
@@ -24,7 +24,7 @@ EOF
 
 sudo apt-get update
 
-sudo apt-get install -y kubelet=1.12.7-00 kubeadm=1.12.7-00 kubectl=1.12.7-00
+sudo apt-get install -y kubelet=1.13.7-00 kubeadm=1.13.7-00 kubectl=1.13.7-00
 
 sudo apt-mark hold kubelet kubeadm kubectl
 
